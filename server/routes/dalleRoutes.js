129db6ae84079ca2c8d9e481e17c6bb834f635ed
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-02-13 20:36:51 +0530

    first commit

---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotennv from 'dotenv'
+import { Configuration, OpenAIApi} from "openai";
+
+
+
+dotennv.config();
+
+const router = express.Router();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY, 
+})
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req,res)=>{
+    res.status(200).json({ message: 'Hello from DALL-E!' });
+})
+
+router.route('/').post(async(req,res)=>{
+    try{
+        const { prompt } = req.body;
+        //console.log(req.body);
+
+        const aiResponce = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json',
+        });
+
+        const image = aiResponce.data.data[0].b64_json;
+
+        res.status(200).json({ photo: image });
+    }catch(err){
+        //console.log(err);
+        res.status(500).send(err?.response.data.error.message)
+    }
+})
+
+export default router;
+
